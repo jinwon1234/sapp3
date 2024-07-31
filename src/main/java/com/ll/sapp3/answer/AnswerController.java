8aebe2d8906b9d2f8a8d1dabb3ee392cc73cd39d
jinwon1234 <ljw0626@naver.com>
2024-07-31 09:41:59 +0900

    work

---
+++ src/main/java/com/ll/sapp3/answer/AnswerController.java
@@ -0,0 +1,37 @@
+package com.ll.sapp3.answer;
+
+
+import com.ll.sapp3.question.Question;
+import com.ll.sapp3.question.QuestionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content
+    ,@Valid AnswerForm answerForm, BindingResult bindingResult) {
+        Question question = this.questionService.getQuestion(id);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
+
+
+}
---
+++ src/main/java/com/ll/sapp3/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sapp3.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
